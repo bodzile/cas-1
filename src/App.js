b60d405fca2a5d491996abc8214bf2e9a83e4469
bodzile <155837912+bodzile@users.noreply.github.com>
2026-05-03 19:31:32 +0200

    Vezbanje 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 
 import "./style.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import Products from "./Components/Products"
 import { useState } from "react";
 
-function clickOnButton(){
-  console.log("radi");
-}
 
 function App() {
 
-  let [name,setName]=useState("Hello world");
   let [tax, setTax] = useState(0);
 
-  const changeName = e => setName(e.target.value);
   const changeTax = e => setTax(e.target.value);
 
 
   return (
     <>
       <Products tax={tax}/>
+        <div className="p-3">
+            <h4>Unesite taksu: </h4>
+            <input placeholder='taksa' className="form-control w-25" type="text" onInput={changeTax} />
+        </div>
 
-      <button onClick={clickOnButton}>{ name }</button>
-
-      <input type="text" onInput={changeName} />
-        <input type="text" onInput={changeTax} />
     </>
   );
 }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,12 +6,14 @@ function Products(props){
 
     let [products, setProducts] = useState({
         "iPhone 14" : 1000,
-        "iPhone15" : 1500,
+        "iPhone 15" : 1500,
         "Samsung s23 Ultra": 2300
     });
 
-    let [newProductName, setNewProductName] = useState();
-    let [newProductPrice, setNewProductPrice] = useState();
+    let [newProductName, setNewProductName] = useState("");
+    let [newProductPrice, setNewProductPrice] = useState("");
+
+    let [infoMessage, setInfoMessage] = useState("");
 
     function deleteProducts(e){
         setProducts({});
@@ -19,26 +21,63 @@ function Products(props){
     }
 
     function addProduct(e){
+
+        if(newProductName === "" || newProductPrice === "")
+            return;
+
         e.target.closest("div").querySelectorAll("input").forEach(e=> e.value="");
-        setProducts({
-            ...products,
+
+        setProducts(old => ({
+            ...old,
             [newProductName]: newProductPrice
-        });
+        }));
     }
 
+    function search(e){
+
+        const searchTerm = e.target.value.toLowerCase();
+        let productNames = Object.keys(products);
+
+        for(let product in productNames){
+            let productName = productNames[product].toLowerCase();
+            if(searchTerm === productName){
+                setInfoMessage("Postoji proizvod");
+                return;
+            }
+        }
+        setInfoMessage("Ne postoji proizvod");
+
+    }
 
     return (
-        <>
-            <button onClick={deleteProducts}>Delete products</button>
-            {Object.entries(products).map( ([phone,price]) => {
-                return <p key={phone}>{ phone } , price: ${ CalculateTax(price,props.tax) } </p>
-            })}
-            <div>
-                <input onInput={e => setNewProductName(e.target.value)} placeholder="Unesite ime proizvoda" type="text"/>
-                <input onInput={e => setNewProductPrice(e.target.value)} placeholder="Unesite cenu proizvoda" type="number"/>
-                <button onClick={addProduct}>Add new product</button>
+        <div className="p-3">
+            <h2>All products</h2>
+            <button className="btn btn-danger" onClick={deleteProducts}>Delete products</button>
+            <div className="d-flex m-2 gap-3 ">
+                {Object.entries(products).map( ([phone,price]) => (
+                    <div>
+                        <h3>{phone}</h3>
+                        <p>${ CalculateTax(price,props.tax) }</p>
+                    </div>
+                ))}
             </div>
-        </>
+            <p>{infoMessage}</p>
+            <div>
+                <input className="w-25 form-control" onInput={search} type="text" placeholder="Unesite ime za pretragu"/>
+            </div>
+            <div className="mt-3 w-25 d-flex flex-column gap-2">
+                <h3>Dodavanje proizvoda</h3>
+                <div>
+                    <label className="form-label" htmlFor="">Ime proizvoda:</label>
+                    <input className="form-control" onInput={e => setNewProductName(e.target.value)} placeholder="Unesite ime proizvoda" type="text"/>
+                </div>
+                <div>
+                    <label className="form-label" htmlFor="">Cena proizvoda:</label>
+                    <input className="form-control" onInput={e => setNewProductPrice(e.target.value)} placeholder="Unesite cenu proizvoda" type="number"/>
+                </div>
+                <button className="btn btn-success" onClick={addProduct}>Add new product</button>
+            </div>
+        </div>
     );
 }
 
